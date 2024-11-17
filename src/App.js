a2713c8d71923394d885392dd760bb21605a8f75
Eva Machova <eva.machova1997@gmail.com>
2024-11-17 13:18:39 +0000

    Added links in the footer of the mockup app

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <div className="container">
           <Form />
           <Weather />
-          This project was coded by <a href="https://www.linkedin.com/in/eva-machova/">Eva Machova</a>, is open-sourced and hosted on Netlify.
+          This project was coded by <a href="https://www.linkedin.com/in/eva-machova/">Eva Machova</a>, is <a href="https://github.com/eva-coding/shecodes-react-weather">open-sourced</a> and hosted on <a href="https://grand-truffle-3f89c7.netlify.app/">Netlify</a>.
         </div>
     </div>
   );
